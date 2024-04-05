293b4ecaab2a129212d6f8045cafb1abb2f2bd34
Chijioke Divine <108485884+ChijiokeDivine@users.noreply.github.com>
2024-04-05 01:18:45 +0100

    ...

--- martmesproj/urls.py
+++ martmesproj/urls.py
@@ -34,3 +34,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler400 = 'core.views.error_400'
+handler404 = 'core.views.error_404'
+handler403 = 'core.views.error_403'
+handler500 = 'core.views.error_500'
